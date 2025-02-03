eff1cfc8cdb62b3d29287a1a8c75e7f3225720fe
damccull <401104+damccull@users.noreply.github.com>
2025-02-03 11:59:11 -1000

    Context working in dioxus

--- src/common.rs
+++ src/common.rs
@@ -7,30 +7,16 @@ use dioxus::prelude::*;
 #[server(endpoint = "backend_list_peers")]
 #[tracing::instrument(skip_all)]
 pub async fn count_peers() -> Result<u32, ServerFnError> {
-    tracing::debug!("In count_peers");
     use datastore::Datastore;
-    use dioxus_elements::data;
 
     use crate::protocols::b1::B1Datastore;
 
-    tracing::debug!("Get datastore");
-    //let FromContext::<Datastore>(datastore) = extract().await?;
-    let x = extract().await;
-    let datastore = match x {
-        Ok(FromContext::<Datastore>(ds)) => {
-            tracing::debug!("Got datastore {:#?}", &ds);
-            ds
-        }
-        Err(e) => {
-            tracing::debug!("Can't get datastore: {:#?}", e);
-            return Err(ServerFnError::new(e));
-        }
-    };
-
-    tracing::debug!("Got datastore {:?}", &datastore);
+    tracing::debug!("Trying to get datastore from dioxus context");
+    let FromContext::<Datastore>(datastore) = extract().await?;
 
     let datastore: B1Datastore = datastore.into();
-    tracing::debug!("Getting peer count");
     let peers = datastore.count_peers().await.map_err(ServerFnError::new)?;
+    tracing::debug!("Peer count: {}", &peers);
+
     Ok(peers)
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 #![cfg(feature = "server")]
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc};
 
 use axum::{extract::FromRef, response::IntoResponse, routing::get};
 use dioxus::prelude::*;
@@ -19,21 +19,33 @@ pub async fn run(settings: Settings) -> anyhow::Result<()> {
     tracing::info!("Connecting to database");
     let db = settings.database.get_db().await?;
 
+    let axum_app_state = AppState {
+        datastore: db.clone(),
+    };
+
+    // TODO: Replace this mess with `.context()` when released in future:
+    // https://github.com/DioxusLabs/dioxus/pull/3483
+    let dioxus_server_app_state = Arc::new(vec![Box::new({
+        let local_state = axum_app_state.datastore.clone();
+        move || Box::new(local_state.clone()) as Box<dyn std::any::Any>
+    })
+        as Box<dyn Fn() -> Box<dyn std::any::Any> + Send + Sync + 'static>]);
+
+    tracing::debug!("APPSTATE: {:#?}", &axum_app_state);
     let (chain_message_tx, _chain_message_rx) = mpsc::channel::<ChainMessage>();
 
     tracing::info!("Launching web and API server");
-    let app = axum::Router::new().serve_dioxus_application(ServeConfigBuilder::new(), App);
+    let app = axum::Router::new().serve_dioxus_application(
+        ServeConfigBuilder::new().context_providers(server_only!(dioxus_server_app_state)),
+        App,
+    );
 
     let b1 = B1Protocol::initialize(db.clone(), settings.clone(), chain_message_tx.clone());
 
     tokio::spawn(async move { b1.run().await });
-    let appstate = AppState {
-        datastore: db.clone(),
-    };
-    tracing::debug!("APPSTATE: {:#?}", &appstate);
 
     let app = B1Protocol::register_routes(app).route("/health_check", get(health_check));
-    let app = app.with_state(appstate);
+    let app = app.with_state(axum_app_state);
 
     let socket_address = dioxus_cli_config::fullstack_address_or_localhost();
     let listener = TcpListener::bind(&socket_address).await.unwrap();
