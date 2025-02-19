de4cf7b6d7428f4bf39ab31df4ebf68b0586969f
damccull <401104+damccull@users.noreply.github.com>
2025-02-18 20:15:45 -1000

    Update flake.nix to set wasm-bindgen-cli to version to 0.2.99

--- flake.nix
+++ flake.nix
@@ -103,6 +103,20 @@
               pkg-config
               rustPlatform.bindgenHook
               stdenv.cc.cc.lib
+              (wasm-bindgen-cli.overrideAttrs (oldAttrs: rec {
+                version = "0.2.99";
+                src = fetchCrate {
+                  pname = "wasm-bindgen-cli";
+                  version = "0.2.99";
+                  hash = "sha256-1AN2E9t/lZhbXdVznhTcniy+7ZzlaEp/gwLEAucs6EA=";
+                };
+
+                cargoDeps = rustPlatform.fetchCargoVendor {
+                  inherit src;
+                  inherit (src) pname version;
+                  hash = "sha256-HGcqXb2vt6nAvPXBZOJn7nogjIoAgXno2OJBE1trHpc=";
+                };
+              }))
             ]
             ++ dioxusDeps
             ++ (if enableAndroid then androidDeps else [ ]);
