ce6b052a5deb7c15336c60a7ba497b5e1b8be6d2
damccull <401104+damccull@users.noreply.github.com>
2025-03-10 14:39:09 -1000

    Pull live data for peer_count endpoint

--- src/protocols/b1/server/client_api.rs
+++ src/protocols/b1/server/client_api.rs
@@ -1,16 +1,13 @@
 use axum::{extract::State, response::IntoResponse, Json};
 
-use crate::common::{datastore::Datastore, ResponseError};
+use crate::common::ResponseError;
 
 use super::B1Datastore;
 
 pub async fn get_peer_count(
     State(datastore): State<B1Datastore>,
 ) -> Result<impl IntoResponse, ResponseError> {
-    //let datastore: B1Datastore = datastore.into();
-    //
-    //let peer_count = datastore.peer_count().await?;
-    let peer_count = 0u32;
+    let peer_count = datastore.peer_count().await?;
 
     Ok(Json(peer_count))
 }
