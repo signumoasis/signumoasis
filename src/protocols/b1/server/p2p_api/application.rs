182fd5a9a9e1ef7c79553b8c2ec081abfd94172f
damccull <401104+damccull@users.noreply.github.com>
2025-03-27 17:01:57 -1000

    API base handler working

--- src/protocols/b1/server/p2p_api/application.rs
+++ src/protocols/b1/server/p2p_api/application.rs
@@ -1,4 +1,11 @@
-use axum::{routing::IntoMakeService, serve::Serve, Router};
+use axum::{
+    extract::FromRef,
+    routing::{get, post, IntoMakeService},
+    serve::Serve,
+    Router,
+};
+use http::StatusCode;
+use surrealdb::method::Health;
 use tokio::net::TcpListener;
 
 use crate::protocols::b1::{B1Datastore, B1Settings};
@@ -50,12 +57,36 @@ pub struct ApplicationBaseUrl(pub String);
 
 async fn run(
     listener: TcpListener,
-    db: B1Datastore,
+    datastore: B1Datastore,
     settings: B1Settings,
 ) -> Result<AppServer, anyhow::Error> {
-    let app = Router::new().route("/", axum::routing::get(|| async { "Hello, World!" }));
+    let app_state = B1AppState {
+        datastore,
+        settings,
+    };
+    let app = Router::new()
+        .route("/health_check", get(StatusCode::OK))
+        .route("/", post(signum_api_handler))
+        .with_state(app_state);
 
     let server = axum::serve(listener, app.into_make_service());
 
     Ok(server)
+}
+
+#[derive(Clone, Debug)]
+struct B1AppState {
+    datastore: B1Datastore,
+    settings: B1Settings,
+}
+
+impl FromRef<B1AppState> for B1Datastore {
+    fn from_ref(input: &B1AppState) -> Self {
+        input.datastore.clone()
+    }
+}
+impl FromRef<B1AppState> for B1Settings {
+    fn from_ref(input: &B1AppState) -> Self {
+        input.settings.clone()
+    }
 }
--- src/protocols/b1/server/p2p_api/request_models.rs
+++ src/protocols/b1/server/p2p_api/request_models.rs
@@ -1,25 +1,70 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, skip_serializing_none, DisplayFromStr};
 
 /// Represents each of the types of request that can be made to the SRS Peer to Peer API.
 /// Currently ignores the 'protocol' field, since that is always `B1` and has never changed.
 /// May need to include that later if SRS changes.
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde_as]
 #[serde(rename_all = "camelCase")]
 #[serde(rename_all_fields = "camelCase")]
 #[serde(tag = "requestType")]
 pub enum RequestType {
     AddPeers { peers: Vec<String> },
-    GetInfo(GetInfoRequestModel),
-    GetPeers {},
+    GetBlocksFromHeight(GetBlocksFromHeightPayload),
+    GetCumulativeDifficulty,
+    GetInfo(GetInfoPayload),
+    GetMilestoneBlockIds(GetMilestoneBlockIdsPayload),
+    GetNextBlocks(GetNextBlocksPayload),
+    GetNextBlockIds(GetNextBlockIdsPayload),
+    GetUnconfirmedTransactions,
+    GetPeers,
+    ProcessBlock,
+    ProcessTransactions,
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
-pub struct GetInfoRequestModel {
+pub struct GetBlocksFromHeightPayload {
+    pub height: u32,
+    pub num_blocks: u32,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct GetInfoPayload {
     pub announced_address: Option<String>,
     pub application: Option<String>,
     pub version: Option<String>,
     pub platform: Option<String>,
     pub share_address: Option<bool>,
     pub network_name: String,
+}
+
+#[serde_as]
+#[skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct GetMilestoneBlockIdsPayload {
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    pub last_block_id: Option<u64>,
+    #[serde_as(as = "Option<DisplayFromStr>")]
+    pub last_milestone_block_id: Option<u64>,
+}
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct GetNextBlocksPayload {
+    #[serde_as(as = "DisplayFromStr")]
+    pub block_id: u64,
+}
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct GetNextBlockIdsPayload {
+    #[serde_as(as = "DisplayFromStr")]
+    pub block_id: u64,
 }
